53811c47c9f835f4110a8c7a19e65b82226db6f3
quinn95 <quinn95@gmail.com>
2018-05-01 21:06:31 -0500

    not much

--- test.hs
+++ test.hs
@@ -5,16 +5,16 @@ import Control.Monad.Reader (MonadReader, ReaderT, runReaderT)
 import Control.Monad.State (MonadState, StateT, evalStateT)
 import Control.Exception.Safe (MonadThrow, MonadCatch)
 
-type R = Int
+type Config = Int
 
-type Game = Int
+type GameState = Int
 
 
-newtype App a = App (ReaderT R (StateT Game IO) a)
-    deriving ( Functor, Applicative, Monad, MonadReader R, MonadIO, MonadState Game
+newtype App a = App (ReaderT Config (StateT GameState IO) a)
+    deriving ( Functor, Applicative, Monad, MonadReader Config, MonadIO, MonadState GameState
              , MonadThrow, MonadCatch)
 
-runApp :: R -> Game -> App a -> IO a
+runApp :: Config -> GameState -> App a -> IO a
 runApp r g (App m) = evalStateT (runReaderT m r) g
 
 main = putStrLn "Hi"
